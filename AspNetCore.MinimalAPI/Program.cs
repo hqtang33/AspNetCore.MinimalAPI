64aadaf3ef5a883a6cfd91709559e7ebf8a8de7b
haoquantang <hqtang33@gmail.com>
2023-07-13 21:21:46 +0800

    Initial commit

---
+++ AspNetCore.MinimalAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
